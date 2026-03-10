72b41075c703b78095fa449632031f986c5a28d9
jfunnrtjio <181172@noda.ed.jp>
2026-03-10 18:13:19 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,25 +14,16 @@ let timerValue = 60;
 let timerInterval = null;
 
 io.on('connection', (socket) => {
-    // 接続時に最新状態を同期
     socket.emit('init_state', { isLocked, isVisible, timerValue });
 
     socket.on('join', (data) => {
-        // indexが指定されていればそれを使う（display用）、なければ空き番号か連番
-        let targetIndex = data.index;
-        if (targetIndex === undefined) {
-            targetIndex = Object.keys(users).length % 6;
-        }
-        users[socket.id] = { name: data.name, index: targetIndex };
-        
-        // 重要：本人に確定したindexを伝える
-        socket.emit('assigned', { index: targetIndex });
-        // 全員にユーザーリストを更新
+        const index = data.index !== undefined ? data.index : Object.keys(users).length % 6;
+        users[socket.id] = { name: data.name, index: index };
+        socket.emit('assigned', { index: index });
         io.emit('update_users', users);
     });
 
     socket.on('draw', (data) => {
-        // indexが正しく送られてきている場合のみ、全員（display含む）に描画を転送
         if (!isLocked && data.index !== null) {
             io.emit('render', { index: data.index, image: data.image });
         }
